e44ff83f0cbcef8ebbeccab26ca69080e3c91abe
Marc <marcplanella27@gmail.com>
2025-07-19 21:23:36 +0200

    public/js

---
+++ frontend/public/js/main.js
@@ -0,0 +1,18 @@
+import "./components/modalHandler.js";
+import "./services/localStorageHandler.js";
+import "./ui/themeToggle.js";
+import "./ui/headerToggle.js";
+import "./validation/carFormValidation.js";
+
+
+
+
+
+
+
+
+
+
+
+
+
